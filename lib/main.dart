e075a3d7490948a49f43f82292f235a162e12718
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-12-12 23:31:21 +0530

    container widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+/*
+// scaffold ,appBar ,MaterislApp , Icons , Colors widgets 
+
 import 'package:flutter/material.dart'; // we can use free build widgets
  
 void main(){
@@ -18,6 +21,76 @@ class MyApp extends StatelessWidget {
       ) ,
 
       body:const Icon(Icons.add), // add + icon
+      ),
+    );
+  }
+}
+*/
+
+
+
+// continer widget 
+
+
+/*
+import 'package:flutter/material.dart';
+ 
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Space App",
+      home: Scaffold(
+        body: Container(
+          height: 250,
+          width: 250,  
+          child: Text("Hello"), 
+          decoration:BoxDecoration(
+          borderRadius: BorderRadius.circular(1000),
+          color: Colors.red, 
+           ),
+        ),
+      ),
+    );
+  }
+}
+
+*/
+
+
+
+
+// continer widget 
+
+import 'package:flutter/material.dart';
+ 
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Space App",
+      home: Scaffold(
+        body: Container(
+          height: 250,
+          width: 250,  
+          child: Text("Hello"), 
+          decoration:BoxDecoration(
+          borderRadius: BorderRadius.circular(1000),
+          color: Colors.red, 
+           ),
+        ),
       ),
     );
   }
